d8d50cab872fa4eceae24c87fb8c815de8e2bfcf
Brandon Borden <bb2790@nyu.edu>
2018-11-24 21:51:02 -0500

    Version 1: Rotary and FM
    Working!

--- final_project.ino
+++ final_project.ino
@@ -1,10 +1,136 @@
+
+#include <Encoder.h>
+#include <Wire.h>
+#include <Adafruit_Si4713.h>
+#define RESETPIN 34
+#define FMSTATION freqVal      // 10230 == 102.30 MHz
+Adafruit_Si4713 radio = Adafruit_Si4713(RESETPIN);
+
+Encoder myEnc (31, 32);
+int newPosition = 0;
+int oldPosition = 0;
+int freqVal = 0;
+
+
+
 void setup() {
-  // put your setup code here, to run once:
+  /**
+     FM transmitter setup
+  */
+  Serial.begin(9600);
+  Serial.println("Adafruit Radio - Si4713 Test");
+
+  if (! radio.begin()) {  // begin with address 0x63 (CS high default)
+    Serial.println("Couldn't find radio?");
+    while (1);
+  }
+
+  // Uncomment to scan power of entire range from 87.5 to 108.0 MHz
+
+  for (uint16_t f  = 8750; f < 10800; f += 10) {
+    radio.readTuneMeasure(f);
+    Serial.print("Measuring "); Serial.print(f); Serial.print("...");
+    radio.readTuneStatus();
+    Serial.println(radio.currNoiseLevel);
+  }
+
+
+  Serial.print("\nSet TX power");
+  radio.setTXpower(115);  // dBuV, 88-115 max
+
+  Serial.print("\nTuning into ");
+  Serial.print(FMSTATION / 100);
+  Serial.print('.');
+  Serial.println(FMSTATION % 100);
+  radio.tuneFM(FMSTATION); // 102.3 mhz
+
+  // This will tell you the status in case you want to read it from the chip
+  radio.readTuneStatus();
+  Serial.print("\tCurr freq: ");
+  Serial.println(radio.currFreq);
+  Serial.print("\tCurr freqdBuV:");
+  Serial.println(radio.currdBuV);
+  Serial.print("\tCurr ANTcap:");
+  Serial.println(radio.currAntCap);
+
+  // begin the RDS/RDBS transmission
+  radio.beginRDS();
+  radio.setRDSstation("AdaRadio");
+  radio.setRDSbuffer( "Adafruit g0th Radio!");
+
+  Serial.println("RDS on!");
+
+  radio.setGPIOctrl(_BV(1) | _BV(2));  // set GP1 and GP2 to output
+  /**
+     Rotary Encoder Stuff
+  */
+  Serial.println("Basic Encoder Test:");
 
 }
 
-void loop() {
-  for (int i <
-  // put your main code here, to run repeatedly:
 
+
+void loop() {
+  /**
+     FM loop
+
+  */
+  delay (2000);
+  radio.readASQ();
+  Serial.print("\tCurr ASQ: 0x");
+  Serial.println(radio.currASQ, HEX);
+  Serial.print("\tCurr InLevel:");
+  Serial.println(radio.currInLevel);
+  // toggle GPO1 and GPO2
+  radio.setGPIO(_BV(1));
+  delay(500);
+  radio.setGPIO(_BV(2));
+  delay(500);
+  /**
+     Rotary Encoder Loop
+  */
+  encoderFunc();
+}
+
+
+
+void encoderFunc() {
+
+  newPosition = myEnc.read();
+
+  if (newPosition >= oldPosition + 4) {
+    oldPosition = newPosition;
+    freqVal = oldPosition / 4 * (10);
+    Serial.println("forward");
+    Serial.println(freqVal);
+    setFMmodule();
+  } else if (newPosition <= oldPosition - 4) {
+    oldPosition = newPosition;
+    freqVal = oldPosition / 4 * (1000);
+    Serial.println("backwards");
+    Serial.println(freqVal);
+    setFMmodule();
+
+  }
+  if (freqVal > 10800) {
+    freqVal = 8750;
+    setFMmodule();
+  } else if (freqVal > 8750) {
+    freqVal = 10800;
+    setFMmodule();
+  } else if (freqVal == 0) {
+    freqVal = 8750;
+    setFMmodule();
+  }
+}
+
+void setFMmodule () {
+  Serial.print("\nSet TX power");
+  radio.setTXpower(115);  // dBuV, 88-115 max
+
+  Serial.print("\nTuning into ");
+  Serial.print(FMSTATION / 100);
+  Serial.print('.');
+  Serial.println(FMSTATION % 100);
+  radio.tuneFM(FMSTATION); // 102.3 mhz
 }
